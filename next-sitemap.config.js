8608186be65e8507c62d9617921ee14488b245f2
okuwabara07-gif <o.kuwabara07@gmail.com>
2026-04-05 16:24:08 +0900

    feat: add next-sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,10 @@
 module.exports = {
   siteUrl: 'https://kcos-review-jp.vercel.app',
   generateRobotsTxt: true,
-  outDir: './public',
-};
+  robotsTxtOptions: {
+    additionalSitemaps: ['https://kcos-review-jp.vercel.app/sitemap.xml'],
+  },
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+}
